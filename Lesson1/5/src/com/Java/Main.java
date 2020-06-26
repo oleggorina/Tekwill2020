9230db0fc05e491b559fa2cc7faefc7644c2c3a8
Oleg Gorina <oleg.gorina@gmail.com>
2020-06-26 14:08:14 +0300

    My first commit

---
+++ Lesson1/5/src/com/Java/Main.java
@@ -0,0 +1,10 @@
+package com.Java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("O X X");
+        System.out.println("O X O");
+        System.out.println("X O X");
+    }
+}
