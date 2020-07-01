3f3a8d4a454430ffa2b7d8f37362e2726227829c
Oleg Gorina <oleg.gorina@gmail.com>
2020-07-01 13:54:22 +0300

    Lesson 3

---
+++ Lesson3/3.2/src/com/Java/Main.java
@@ -0,0 +1,10 @@
+package com.Java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 512343;
+        int b = 3431231;
+        System.out.println(a + b);
+    }
+}
---
+++ Lesson3/3.3/src/com/Java/Main.java
@@ -0,0 +1,9 @@
+package com.Java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 123456;
+        System.out.println(a);
+    }
+}
